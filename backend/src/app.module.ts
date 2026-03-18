4e1fb2f038ba3a3c2e8b242292a7f2145465b714
jakobhonstad <jakobonstad@gmail.com>
2026-03-18 19:48:44 +0100

    Slettet unødevendige filer

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { DatabaseModule } from './database/database.module';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
   imports: [GameModule, DatabaseModule, SongsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
