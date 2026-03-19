3f67ffb5295ab0e65a4d5ceeaf06ad1d74553c8e
jakobhonstad <jakobonstad@gmail.com>
2026-03-19 12:48:37 +0100

    fikset indent feil i fil

--- backend/src/game/game-create.service.ts
+++ backend/src/game/game-create.service.ts
@@ -3,35 +3,35 @@ import { DatabaseService } from "src/database/database.service";
 import { CreateGameDto } from "./dto/game.dto";
 
 function generateGameCode(): string {
-    return Math.random().toString(36).substring(2, 8).toUpperCase();
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
 };
 
 @Injectable()
 export class GameCreateService {
-    constructor(private prisma: DatabaseService) { }
+  constructor(private prisma: DatabaseService) { }
 
-    async createGame(createGameDto: CreateGameDto, hostId: string) {
-        const code = generateGameCode();
+  async createGame(createGameDto: CreateGameDto, hostId: string) {
+    const code = generateGameCode();
 
-        const game = await this.prisma.game.create({
-            data: {
-                code,
-                hostId,
-                category: createGameDto.category,
-                maxRounds: createGameDto.maxRounds || 10,
-                players: {
-                    create: {
-                        id: hostId,
-                        name: createGameDto.hostName,
-                        isHost: true,
-                    }
-                }
-            },
-            include: {
-                players: true,
-                rounds: true
-            }
-        })
-        return game;
-    }
+    const game = await this.prisma.game.create({
+      data: {
+        code,
+        hostId,
+        category: createGameDto.category,
+        maxRounds: createGameDto.maxRounds || 10,
+        players: {
+          create: {
+            id: hostId,
+            name: createGameDto.hostName,
+            isHost: true,
+          }
+        }
+      },
+      include: {
+        players: true,
+        rounds: true
+      }
+    })
+    return game;
+  }
 };
